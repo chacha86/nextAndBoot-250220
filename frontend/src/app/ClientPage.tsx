546cc926a0a54cc8f3c549974563a0cbf959a812
chacha86 <taejincha0619@gmail.com>
2025-02-24 10:58:39 +0900

    kakao button url link

--- frontend/src/app/ClientPage.tsx
+++ frontend/src/app/ClientPage.tsx
@@ -5,11 +5,16 @@ import { use } from "react";
 
 export default function Page() {
   const { isLogin, loginMember } = use(LoginMemberContext);
+
   return (
     <>
       {!isLogin && (
         <div className="flex flex-grow justify-center items-center">
-          <Button>카카오 로그인</Button>
+          <Button>
+            <a href="http://localhost:8080/oauth2/authorization/kakao?redirectUrl=localhost:3000">
+              카카오 로그인
+            </a>
+          </Button>
         </div>
       )}
       {isLogin && <div>{loginMember.nickname}님 환영합니다.</div>}
