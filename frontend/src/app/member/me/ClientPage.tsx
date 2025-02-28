326fee77c13a8ca4d421bcd1419dede6b3b546c9
chacha86 <taejincha0619@gmail.com>
2025-02-28 12:16:47 +0900

    Image Tag

--- frontend/src/app/member/me/ClientPage.tsx
+++ frontend/src/app/member/me/ClientPage.tsx
@@ -2,6 +2,7 @@
 
 import { LoginMemberContext } from "@/stores/auth/loginMemberStore";
 import { use } from "react";
+import Image from "next/image";
 
 export default function ClinetPage() {
   const { loginMember } = use(LoginMemberContext);
@@ -9,8 +10,17 @@ export default function ClinetPage() {
   return (
     <>
       <div>내정보 페이지</div>
-      <div>번호 : {loginMember.id}</div>
       <div>닉네임 : {loginMember.nickname}</div>
+      <div>
+        <Image
+          className="w-20 h-20 rounded-full"
+          src={loginMember.profileImgUrl}
+          alt="프로필 이미지"
+          width={80}
+          height={80}
+          quality={100}
+        />
+      </div>
     </>
   );
 }
