5f7927b6a40a8db92955ba213587f452dd64208c
chacha86 <taejincha0619@gmail.com>
2025-03-10 08:46:27 +0900

    sorting

--- backend/src/main/java/com/example/next/domain/post/post/repository/CustomPostRepositoryImpl.java
+++ backend/src/main/java/com/example/next/domain/post/post/repository/CustomPostRepositoryImpl.java
@@ -1,8 +1,12 @@
 package com.example.next.domain.post.post.repository;
 
+import com.example.next.domain.post.post.controller.SearchKeywordType;
 import com.example.next.domain.post.post.dto.PostListParamDto;
 import com.example.next.domain.post.post.entity.Post;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
@@ -20,12 +24,20 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
     @Override
     public Page<Post> findByParam(PostListParamDto postListParamDto, Pageable pageable) {
 
+        String keyword = postListParamDto.keyword();
+        SearchKeywordType keywordType = postListParamDto.keywordType();
+
         BooleanBuilder builder = new BooleanBuilder();
         if (postListParamDto.listed() != null) {
             builder.and(post.listed.eq(postListParamDto.listed()));
         }
         if (postListParamDto.published() != null) {
             builder.and(post.published.eq(postListParamDto.published()));
+        }
+
+        switch (keywordType) {
+            case title -> builder.and(post.title.containsIgnoreCase(keyword));
+            case content -> builder.and(post.content.containsIgnoreCase(keyword));
         }
 
         JPAQuery<Post> postJPAQuery = queryFactory.select(post)
@@ -37,6 +49,17 @@ public class CustomPostRepositoryImpl implements CustomPostRepository {
                 .from(post)
                 .where(builder);
 
+        applySorting(pageable, postJPAQuery);
+
         return PageableExecutionUtils.getPage(postJPAQuery.fetch(), pageable, totalQuery::fetchOne);
+    }
+
+    public void applySorting(Pageable pageable, JPAQuery<Post> postJPAQuery) {
+        pageable.getSort().stream()
+                .forEach(order -> {
+                    PathBuilder pathBuilder = new PathBuilder<>(Post.class, "post");
+                    postJPAQuery.orderBy(
+                            new OrderSpecifier(order.getDirection().isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(order.getProperty())));
+                });
     }
 }
